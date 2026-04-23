1811e61635ba6cc9c18d9b22ea0e121a46367e0a
afrzlelah <afrzlelah@gmail.com>
2026-04-23 16:14:01 +0700

    continue dashboard, feat: dashboard page, fix grif

--- src/components/layouts/dashboard/DashboardPage.tsx
+++ src/components/layouts/dashboard/DashboardPage.tsx
@@ -88,7 +88,7 @@ export default function DashboardPage() {
       </section>
 
       {/* Kartu Statistik Cepat */}
-      <section className="grid grid-cols-1 grid-cols-3 gap-6 mb-10">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
         {[1, 2, 3].map((item) => (
           <div
             key={item}
